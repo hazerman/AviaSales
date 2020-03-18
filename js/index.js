fa00aead2fb33de2d7dbbb0f0355a3792e04b42a
hazerman <49827421+hazerman@users.noreply.github.com>
2020-03-18 14:30:22 +0500

    Добавляет поиск и вставку городов

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
-// code here
+
+const formSearch = document.querySelector('.form-search');
+const inputCitiesFrom = formSearch.querySelector('.input__cities-from');
+const dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from');
+const inputCitiesTo = formSearch.querySelector('.input__cities-to');
+const dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to');
+const inputDateDepart = formSearch.querySelector('.input__date-depart');
+
+const cities = ['Абенгуру', 'Джхунджхуну', 'Пунта Кардон', 'Готки', 'Лесной', 'Лион', 'Клагенфурт', 'Квекве',
+  'Беруни', 'Глендейл', 'Михара', 'Цюйцзин', 'Асуль', 'Накхон Си Тхаммарат', 'Бансвара', 'Эспо', 'Наро-Фоминск'];
+
+// Функция вставки городов в инпуты
+
+const pasteCity = (evt, input, dropdown) => {
+  if (evt.target.tagName === 'LI') {
+    input.value = evt.target.textContent;
+    dropdown.textContent = '';
+  }
+};
+
+// Обработчики вставки городов в инпуты
+
+dropdownCitiesFrom.addEventListener('click', (evt) => {
+  pasteCity(evt, inputCitiesFrom, dropdownCitiesFrom);
+});
+
+dropdownCitiesTo.addEventListener('click', (evt) => {
+  pasteCity(evt, inputCitiesTo, dropdownCitiesTo);
+});
+
+// Функция показа выпадашек городов
+
+const showCities = (input, dropdown) => {
+  dropdown.textContent = '';
+  if (input.value) {
+    const filteredCities = cities.filter((city) => {
+      const cityLowerCase = city.toLowerCase();
+      return cityLowerCase.includes(input.value.toLowerCase());
+    });
+    filteredCities.forEach((city) => {
+      const elementLi = document.createElement('li');
+      elementLi.className = 'dropdown__city';
+      elementLi.setAttribute('tabindex', 0);
+      elementLi.textContent = city;
+      dropdown.append(elementLi);
+    });
+  }
+};
+
+// Обработчики показа выпадашек городов
+
+inputCitiesFrom.addEventListener('input', () => {
+  showCities(inputCitiesFrom, dropdownCitiesFrom);
+});
+
+inputCitiesTo.addEventListener('input', () => {
+  showCities(inputCitiesTo, dropdownCitiesTo);
+});
+
+// Функция для спрятывания выпадашек с условием
+
+const hideCitiesCondition = (evt, dropdown) => {
+  if (evt.relatedTarget && evt.relatedTarget.tagName === 'LI') {
+    return;
+  }
+  dropdown.textContent = '';
+};
+
+// Обработчики при потере фокуса с инпутов
+
+inputCitiesFrom.addEventListener('blur', (evt) => {
+  hideCitiesCondition(evt, dropdownCitiesFrom);
+});
+
+inputCitiesTo.addEventListener('blur', (evt) => {
+  hideCitiesCondition(evt, dropdownCitiesTo);
+});
